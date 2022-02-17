be3c59ff3c2cceb1ce449f24d84cef306d213421
unknown <tedokojikagami@gmail.com>
2022-02-18 06:56:05 +0700

    idk

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Dash from "./Views/MainScreen/DashBoard/Dash";
 import Login from "./Views/Login/Login";
+import Welcome from "./Views/MainScreen/Welcome/Welcome";
 
 function App() {
   return (
  
     <Routes>
-      <Route path="/" element={<Login/>}/>
+      <Route path="/" element={<Welcome/>}/>
+      <Route path="/login" element={<Login/>}/>
       <Route path="/dashboard" element={<Dash/>}/>
     </Routes>
     
---
+++ src/Views/MainScreen/Welcome/Welcome.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Welcome.scss'
+import kevinlogopng from '../../../staticfiles/kevinlol.png'
+import kevinlogopngfull from '../../../staticfiles/fullkevin.jpg'
+import Button from '../../../Assets/Buttons/Button'
+import { useNavigate } from 'react-router-dom'
+function Welcome() {
+  const navi = useNavigate()
+  const navigate = ()=>{
+    navi('/login')
+  }
+  return (
+    <div className='Welcome'>
+        <div className='navbar-wrapper'>
+          <img src={kevinlogopng}/>
+          <div className='buu'>
+          <Button onClick={navigate} className='sb' text="Sign In"/>
+          <Button onClick={navigate} className='sb reg' text="Or Register"/>
+          </div>
+        </div>
+        <img className='w' src={kevinlogopngfull}/>
+    </div>
+  )
+}
+
+export default Welcome

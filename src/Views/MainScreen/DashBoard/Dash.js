b87fccbe7f3bc0873d68bf85463a8fad4c977aef
Tedokoji <91715171+Tedokoji@users.noreply.github.com>
2023-09-19 17:37:22 +0700

    Update Dash.js

--- src/Views/MainScreen/DashBoard/Dash.js
+++ src/Views/MainScreen/DashBoard/Dash.js
@@ -35,7 +35,7 @@ function Dash() {
    </div>
   { nav && <div className="navmenu" onMouseLeave={()=>{setNav(false)}}>
         <div className="nav-text" onClick={()=>{
-            if(auth.currentUser?.email === "chauhhuynhanhtu@gmail.com"){
+            if(auth.currentUser?.email === "violetevergarden137@gmail.com"){
                navi('/createquiz')
             }
             
